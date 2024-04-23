3bf4873b1f38e9a3e73f5e9cd34c3f2ccaa96fee
SR <sascha.rienacker@lpp.polytechnique.fr>
2024-04-23 10:42:37 +0200

    update type in mpl style name

--- plotfactory/__init__.py
+++ plotfactory/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 notexstyle = Path(definitions.MPLSTYLE_DIR) / 'notex.mplstyle'
 notex4slidestyle = Path(definitions.MPLSTYLE_DIR) / 'notex4slide.mplstyle'
 texstyle = Path(definitions.MPLSTYLE_DIR) / 'tex.mplstyle'
-tex4slidesstyle = Path(definitions.MPLSTYLE_DIR) / 'tex4slides.mplstyle'
+tex4slidestyle = Path(definitions.MPLSTYLE_DIR) / 'tex4slides.mplstyle'
 notexposterstyle = Path(definitions.MPLSTYLE_DIR) / 'notexposter.mplstyle'
 notexposte = Path(definitions.MPLSTYLE_DIR) / 'notexposter.mplstyle'
 
