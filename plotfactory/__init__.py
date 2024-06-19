b23109b70dd05063c2a42d4d27dae96a5debd4c4
SR <sascha.rienacker@lpp.polytechnique.fr>
2024-06-19 12:53:23 +0200

    forgot to modify __init__.py

--- plotfactory/__init__.py
+++ plotfactory/__init__.py
@@ -6,8 +6,8 @@ notexstyle = Path(definitions.MPLSTYLE_DIR) / 'notex.mplstyle'
 notex4slidestyle = Path(definitions.MPLSTYLE_DIR) / 'notex4slide.mplstyle'
 texstyle = Path(definitions.MPLSTYLE_DIR) / 'tex.mplstyle'
 tex4slidestyle = Path(definitions.MPLSTYLE_DIR) / 'tex4slides.mplstyle'
+texposterstyle = Path(definitions.MPLSTYLE_DIR) / 'texposter.mplstyle'
 notexposterstyle = Path(definitions.MPLSTYLE_DIR) / 'notexposter.mplstyle'
-notexposte = Path(definitions.MPLSTYLE_DIR) / 'notexposter.mplstyle'
 
 from plotfactory.src import plotting
 from plotfactory.src.plotting import join_axes, join_xaxes, join_yaxes
